b102e170ef92967e337d83b517f0d149d582029d
InventamSDE-1 <inventam.sde1@gmail.com>
2021-03-18 15:39:57 +0530

    spacy with nlu

---
+++ python test/test.py
@@ -0,0 +1,34 @@
+# filedata = open('data/myfile.txt');
+
+# print(filedata.readlines())
+
+# control flow
+# test = False
+# test2 = False
+
+# if test:
+#     print("tested")
+
+# elif test2:
+#     print("False hitted")
+# else:
+#     print("final else block")
+
+
+
+
+# for loops in python.
+
+# a = "akshay"
+# print(a[1])
+# print(a.split(" "))
+
+# for index, word in  enumerate(a):
+#     print(index,word)
+
+
+# def testFunction(*kids):
+#     for i in kids:
+#         print(i) 
+
+# testFunction("email","tested")
